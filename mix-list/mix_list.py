335c73cbae99181e5c3e079c3613a7e4bdd525e4
ibrahim <xibrahimx440@gmail.com>
2019-06-10 11:15:35 -0400

    mixlist dir changed

---
+++ mix-list/mix_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+my_list = ['192.168.0.5', 5060,'UP']
+
+print(f'my first item is (IP): {my_list[0]}')
+print(f'my second item is (port): {my_list[1]}')
+print(f'my third item is (state): {my_list[2]}')
+
