e596fb4b3344ce2c85b51b2bdd40e2225861b5bb
ibrahim <xibrahimx440@gmail.com>
2019-06-10 13:24:28 -0400

    went further

--- mix-list/mix_list.py
+++ mix-list/mix_list.py
@@ -6,3 +6,7 @@ print(f'my first item is (IP): {my_list[0]}')
 print(f'my second item is (port): {my_list[1]}')
 print(f'my third item is (state): {my_list[2]}')
 
+
+new_list = [5060, '80', 55, '10.0.0.1', '10.20.30.1', 'ssh']
+
+print(f'when i {new_list[5]}, into IP addresses {new_list[3]} or {4} I ams am ubable to ping ports {new_list[0]},{new_list[1]}, or {new_list[2]}.')
